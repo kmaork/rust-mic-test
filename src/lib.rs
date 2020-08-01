765a262a637bc4ea7bc42cdd432a4111d2a59d82
Maor Kleinberger <kmaork@gmail.com>
2020-08-01 16:29:22 +0300

    Added mictest struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 #![feature(associated_type_bounds)]
 
 use plotters::prelude::*;
+// use cpal::Host;
+use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
 use std::error::Error;
+use std::sync::mpsc::sync_channel;
+use cpal::DeviceNameError;
 
-pub fn draw<DB: DrawingBackend>(backend: DB, points: &[f32], caption: &str) -> Result<(), Box<dyn Error>>
+mod rolling;
+
+pub fn draw_mic_input_on_plotters_backend<DB: DrawingBackend>(backend: DB, points: &[f32], caption: &str) -> Result<(), Box<dyn Error>>
     where <DB as plotters::drawing::DrawingBackend>::ErrorType: 'static {
     let root = backend.into_drawing_area();
     root.fill(&BLACK)?;
@@ -15,3 +21,52 @@ pub fn draw<DB: DrawingBackend>(backend: DB, points: &[f32], caption: &str) -> R
     chart.draw_series(LineSeries::new(data, &GREEN))?;
     Ok(())
 }
+
+pub struct MicTest {
+    device: cpal::Device,
+}
+
+impl MicTest {
+    pub fn new() -> Self {
+        let host = cpal::default_host();
+        let device = host.default_input_device().expect("no input device available"); //: Host::Device
+        Self { device }
+    }
+
+    pub fn device_name(&self) -> Result<String, DeviceNameError> {
+        self.device.name()
+    }
+
+    pub fn start<F: FnMut(&[f32]) -> bool>(&self, mut data_callback: F) -> Result<(), Box<dyn Error>> {
+        let config = self.device
+            .default_input_config()
+            .expect("Failed to get default input config");
+        println!("Default input config: {:?}", config);
+
+        let err_fn = move |err| {
+            eprintln!("an error occurred on stream: {}", err);
+        };
+
+        let (sender, receiver) = sync_channel(100);
+        let stream = self.device.build_input_stream(
+            &config.into(),
+            move |data: &[f32], _: &_| { sender.try_send(data.to_vec()).ok(); },
+            err_fn,
+        )?;
+        stream.play()?;
+
+        let mut roller = rolling::SmoothChunkRoller::new_default(1500);
+        while data_callback(roller.data()) {
+            for chunk in receiver.try_iter() {
+                roller.enqueue(&chunk);
+            }
+            roller.roll();
+        }
+        drop(stream);
+        Ok(())
+    }
+}
+
+// TODO: make faster. Sample rate? ASIO? FPS?
+// TODO: what are channels?
+// TODO: start fast and becomes slower, why?
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,28 @@
 #![feature(associated_type_bounds)]
-use std::sync::mpsc::sync_channel;
-use piston_window::{EventLoop, PistonWindow, WindowSettings};
-use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
-use plotters::prelude::*;
-use rust_mic_test::draw;
 
-mod rolling;
+use piston_window::{EventLoop, PistonWindow, WindowSettings};
+use rust_mic_test::{draw_mic_input_on_plotters_backend, MicTest};
+use std::error::Error;
+use plotters::prelude::draw_piston_window;
+
 
 const FPS: u32 = 30;
 
 
 fn draw_window(mut window: &mut PistonWindow, points: &[f32], caption: &str) -> bool {
-    draw_piston_window(&mut window, |backend| draw(backend, points, caption)).is_some()
+    draw_piston_window(&mut window, |backend| draw_mic_input_on_plotters_backend(backend, points, caption)).is_some()
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let host = cpal::default_host();
-    let device = host.default_input_device().expect("no input device available");
-    let config = device
-        .default_input_config()
-        .expect("Failed to get default input config");
-    println!("Default input config: {:?}", config);
-
-    let err_fn = move |err| {
-        eprintln!("an error occurred on stream: {}", err);
-    };
-
-    let (sender, receiver) = sync_channel(100);
-    let stream = device.build_input_stream(
-        &config.into(),
-        move |data: &[f32], _: &_| { sender.try_send(data.to_vec()).ok(); },
-        err_fn,
-    )?;
-
+fn main() -> Result<(), Box<dyn Error>> {
     let mut window: PistonWindow = WindowSettings::new("rust-mic-test", [600, 400])
         .samples(4)
         .build()
         .expect("Couldn't build window");
 
-    stream.play()?;
     window.set_max_fps(FPS as u64);
     window.set_lazy(false);
     window.set_bench_mode(true);
-    let mut roller = rolling::SmoothChunkRoller::new_default(1500);
-    while draw_window(&mut window, roller.data(), &device.name()?) {
-        for chunk in receiver.try_iter() {
-            roller.enqueue(&chunk);
-        }
-        roller.roll();
-    }
-    drop(stream);
-    Ok(())
+    let mic_test = MicTest::new();
+    let device_name = mic_test.device_name()?;
+    mic_test.start(|data| draw_window(&mut window, data, &device_name))
 }
-
-// TODO: make faster. Sample rate? ASIO? FPS?
-// TODO: what are channels?
